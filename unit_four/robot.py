4736714de98150aa75c8090db2d707bdaae3b40d
shalabyelectronics <mhdshalabystar@gmail.com>
2022-11-23 13:21:40 +0300

    Explain Inheritance and Subclase

--- unit_four/robot.py
+++ unit_four/robot.py
@@ -31,4 +31,22 @@ class Robot:
         return sum(args)
 
 
+class Robotv(Robot):
+    os = "ROS 10"
+    voice_commands_data = {
+            "A":"Arabic",
+            "E":"English",
+            "B":"Arabic & English"
+            }
+    def __init__(self,name,color,lang_support=None):
+        super().__init__(name,color)
+        if lang_support:
+            self.lang_support = lang_support
+        else:
+            self.lang_support = "Not support Voice commands"
+    def voice_commands_info(self):
+        if self.lang_support in Robotv.voice_commands_data.keys():
+            return f"{self.name} Robot can support voice commands in {Robotv.voice_commands_data[self.lang_support]}."
+        return self.lang_support
+
 
