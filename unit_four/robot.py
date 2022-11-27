dc6589c046949a52ec8e948bfcbe12bd414bb582
shalabyelectronics <mhdshalabystar@gmail.com>
2022-11-27 22:47:05 +0300

    Explaining Magic methods

--- unit_four/robot.py
+++ unit_four/robot.py
@@ -33,6 +33,7 @@ class Robot:
 
 class Robotv(Robot):
     os = "ROS 10"
+    robot_length = 100
     voice_commands_data = {
             "A":"Arabic",
             "E":"English",
@@ -44,9 +45,34 @@ class Robotv(Robot):
             self.lang_support = lang_support
         else:
             self.lang_support = "Not support Voice commands"
+        self.my_ro_commands = {}
     def voice_commands_info(self):
         if self.lang_support in Robotv.voice_commands_data.keys():
             return f"{self.name} Robot can support voice commands in {Robotv.voice_commands_data[self.lang_support]}."
         return self.lang_support
+    def __str__(self):
+        return f"Robot name : {self.name}\nRobot color: {self.color}\nRobot Language Support :{self.voice_commands_info()}"
+    def __repr__(self):
+        return f"Robotv(name={self.name},color={self.color},lang_support={self.lang_support})"
+    def __len__(self):
+        return Robotv.robot_length
+    def __setitem__(self,command,replay):
+        if command not in self.my_ro_commands.keys():
+            self.my_ro_commands[command] = replay
+        else:
+            return f"This {command} command is already added"
+    def __getitem__(self,command):
+        if command in self.my_ro_commands.keys():
+            return self.my_ro_commands[command]
+        else:
+            return f"Sorry We can't find this {command} command."
+    def __delitem__(self,command):
+        if command in self.my_ro_commands.keys():
+            del self.my_ro_commands[command]
+            return f"Deleted {command} command is Done."
+        return f"We can't find {command} command to delete it?"
+
+
+
 
 
